6de416c30cf99dd89476b8b54243557eec5d244b
Brian Amesbury <r.brian.amesbury@gmail.com>
2019-12-31 22:11:43 -0700

    lint issues II

--- hosting/src/app/app.module.ts
+++ hosting/src/app/app.module.ts
@@ -6,7 +6,7 @@ import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {NotFoundComponent} from "./not-found/not-found.component";
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   declarations: [
